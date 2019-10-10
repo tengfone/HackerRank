d1fff5d609896e58a9e77bffb14fb37e05b67d28
tengfone <20770447+tengfone@users.noreply.github.com>
2019-10-11 02:34:23 +0800

    Add files via upload

---
+++ JavaProficiency/Java Stdin and Stdout I.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        String myString = scan.next();
+        int myInt = scan.nextInt();
+        scan.close();
+
+        System.out.println(a);
+        System.out.println(myString);
+        System.out.println(myInt);
+    }
+}
+
